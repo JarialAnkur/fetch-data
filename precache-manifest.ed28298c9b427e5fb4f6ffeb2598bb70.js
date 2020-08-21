ec6b645478a8c3129087219324ea565b0bb935e9
Ankur Jarial <ankujarialthakur@gmail.com>
2020-08-21 09:33:31 +0530

    Updates

---
+++ precache-manifest.ed28298c9b427e5fb4f6ffeb2598bb70.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d8c512008238b67d6a415277233550d",
+    "url": "/fetch-data/index.html"
+  },
+  {
+    "revision": "06231dfa42cae6ec9f17",
+    "url": "/fetch-data/static/css/2.98f886d0.chunk.css"
+  },
+  {
+    "revision": "1c3247cfa3271348c241",
+    "url": "/fetch-data/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "06231dfa42cae6ec9f17",
+    "url": "/fetch-data/static/js/2.3f8ed4de.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/fetch-data/static/js/2.3f8ed4de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c3247cfa3271348c241",
+    "url": "/fetch-data/static/js/main.24e18a7c.chunk.js"
+  },
+  {
+    "revision": "6eb2ef544796d3877030",
+    "url": "/fetch-data/static/js/runtime-main.d4c5d310.js"
+  },
+  {
+    "revision": "ec80c6a386f408dd15a071b4b9f43519",
+    "url": "/fetch-data/static/media/india.ec80c6a3.jpg"
+  }
+]);
