3dc48b3739b715047d5a21a7d7af13c32525a4ad
Ankur Jarial <ankujarialthakur@gmail.com>
2020-08-21 09:34:57 +0530

    -3rd commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
-import {Route, Switch, BrowserRouter} from "react-router-dom";
-import {ToastContainer} from "react-toastify";
-import {get} from "axios";
+import {Route} from "react-router-dom";
 import Home from "./components/home"
 import User from "./components/user";
 import NavBar from "./components/navbar";
@@ -11,61 +9,15 @@ import "./App.css";
 
 class App extends Component {
 
-  state={
-    posts : [],
-  }
-
-  handleClick=async ()=>{
-    const{data : posts} = await get("https://jsonplaceholder.typicode.com/posts");
-    this.setState({posts});
-  }
-
-  
-
   render() {
     return (
       <React.Fragment>
       <div>
         <NavBar />
        <div>
-       <ToastContainer/>
         <Route path="/" exact component={Home} />
         <Route path="/User" exact component={User} />
         </div>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Update</th>
-              <th>Delete</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.posts.map((post) => (
-              <tr key={post.id}>
-                <td>{post.title}</td>
-                <td>
-                  <button
-                    className="btn btn-info btn-sm"
-                   
-                  >
-                    Update
-                  </button>
-                </td>
-                <td>
-                  <button
-                    className="btn btn-danger btn-sm"
-                    
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <button onClick={this.handleClick} >Fetch Data</button>
-       
       </div>
     </React.Fragment>
       );
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,10 +1,57 @@
-import React from "react";
 
-const Home=()=>{
-    return(
+import React, { Component } from 'react';
+import { ToastContainer } from "react-toastify";
+import {get} from "axios";
+
+class Home extends Component{
+    state={
+        posts:[],
+    }
+    handleClick=async ()=>{
+        const{data : posts} = await get("https://jsonplaceholder.typicode.com/posts");
+        this.setState({posts});
+      }
+      render(){
+      return(
         <div>
         <h1>This is the home page</h1>
+        
+        <ToastContainer/>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Update</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.posts.map((post) => (
+              <tr key={post.id}>
+                <td>{post.title}</td>
+                <td>
+                  <button
+                    className="btn btn-info btn-sm"
+                   
+                  >
+                    Update
+                  </button>
+                </td>
+                <td>
+                  <button
+                    className="btn btn-danger btn-sm"
+                    
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <button onClick={this.handleClick}  className="btn btn-info btn-sm" >Fetch Data</button>
         </div>
     );
+      }
 }
 export default Home;
